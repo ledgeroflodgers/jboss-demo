2b88c5a842eef8460e62f0310509601281f806e9
ledgeroflodgers <50345923+ledgeroflodgers@users.noreply.github.com>
2021-04-13 16:48:44 -0400

    Update HelloWorldServlet.java

--- src/main/java/com/mastertheboss/servlet/HelloWorldServlet.java
+++ src/main/java/com/mastertheboss/servlet/HelloWorldServlet.java
@@ -12,8 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// http://localhost:8080/helloworld/email
-@WebServlet(name = "email", urlPatterns = { "/email" })
+@WebServlet(name = "email", urlPatterns = { "/call" })
 public class HelloWorldServlet extends HttpServlet {
 
 	public HelloWorldServlet() {
