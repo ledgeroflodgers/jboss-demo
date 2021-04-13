6db0317f82e9f2472d28fb8d1eb93e7899259968
ledgeroflodgers <50345923+ledgeroflodgers@users.noreply.github.com>
2021-04-13 15:47:08 -0400

    Update HelloWorldServlet.java

--- src/main/java/com/mastertheboss/servlet/HelloWorldServlet.java
+++ src/main/java/com/mastertheboss/servlet/HelloWorldServlet.java
@@ -1,6 +1,7 @@
 package com.mastertheboss.servlet;
 
 import com.mastertheboss.servlet.util.GenericSendEmail;
+import com.mastertheboss.servlet.util.Ldap;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -11,7 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "email", urlPatterns = { "/call" })
+// http://localhost:8080/helloworld/email
+@WebServlet(name = "email", urlPatterns = { "/email" })
 public class HelloWorldServlet extends HttpServlet {
 
 	public HelloWorldServlet() {
@@ -33,7 +35,12 @@ public class HelloWorldServlet extends HttpServlet {
 		PrintWriter writer = response.getWriter();
 		writer.println("<h1>Random#: " + int_random + "</h1>");
 		writer.println("<h3>System Property Read test: </h3> <p style=" + "\"color:green;\">" + fileName + "</p>");
+
+		String pass = new Ldap().getPassword();
+		writer.println("<h3>Ldap test: </h3> <p style=" + "\"color:green;\">" + pass + "</p>");
+
 		writer.close();
+
     }
  
 	/**
@@ -43,6 +50,18 @@ public class HelloWorldServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
+	}
+
+	private void sendEmail() {
+		Random rand = new Random(); //instance of random class
+		int upperbound = 250000;
+		int int_random = rand.nextInt(upperbound);
+		try {
+			GenericSendEmail email = new GenericSendEmail();
+			email.sendConfirmationEmail("saeed.dadashi@teranet.ca", "saeeddadashi82@gmail.com", "Hello " + Integer.toString(int_random), "test", null, null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
