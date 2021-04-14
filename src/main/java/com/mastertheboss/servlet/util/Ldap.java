dc30268632408a77cc255a9125a1da026bd8cb7b
ledgeroflodgers <50345923+ledgeroflodgers@users.noreply.github.com>
2021-04-14 10:36:34 -0400

    Update Ldap.java

--- src/main/java/com/mastertheboss/servlet/util/Ldap.java
+++ src/main/java/com/mastertheboss/servlet/util/Ldap.java
@@ -7,10 +7,7 @@ public class Ldap {
     public String getPassword() {
 
         try {
-            //userDN = "uid=csp_bt_super_user,cn=DEV,ou=CSP,ou=private-ldap,dc=teranet-private,dc=ca";
-            //userDN = "uid=system,cn=DEV,ou=CSP,ou=private-ldap,dc=teranet-private,dc=ca";
             String userDN = "uid=csp_bt_super_user,cn=DEV,ou=CSP,ou=private-ldap,dc=teranet-private,dc=ca";
-            //userDN="uid=csp_prod,cn=DEV,ou=CPS,ou=private-ldap,dc=teranet-private,dc=ca";
             return PropsBase.getInstance().getCachedLDAPPasswordByUserDN(userDN);
         } catch (Exception e) {
             System.out.println(e.getMessage());
